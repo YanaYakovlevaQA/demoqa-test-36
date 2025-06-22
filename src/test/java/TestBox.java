dc0150ef41cafc63c37556dfc2aee9131562afc0
Yana Yakovleva <yanna2013yanna@gmail.com>
2025-06-22 16:51:11 +0500

    Add test

--- src/test/java/TestBox.java
+++ src/test/java/TestBox.java
@@ -1,17 +1,17 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestBox {
+
     @Test
     void FillFormTest() {
-        Configuration.timeout = 100000;
+        Configuration.timeout = 10000; // 10 секунд
         Configuration.browserSize = "1920x1980";
         open("https://demoqa.com/text-box");
-        $("#=userName").setValue("Yana");
+        $("#userName").setValue("Yana");
         $("#userEmail").setValue("yana@gmail.com");
         $("#currentAddress").setValue("Some street 1");
         $("#permanentAddress").setValue("Another street 1");
